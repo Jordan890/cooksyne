f21913fccf5cde926f6bc6812c59ccca811e0f0c
Jordan Ahlers <jahlers46@gmail.com>
2026-02-14 12:14:25 -0700

    Working post call

--- adapters/persistence-jpa/build.gradle.kts
+++ adapters/persistence-jpa/build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.30")
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.16.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.0")
 }
 
 tasks.withType<Test> {
