1b8e1b0b6ac7dda2bb50a2b7cf99b13129dbcf0c
Jordan Ahlers <jahlers46@gmail.com>
2026-03-22 12:08:37 -0700

    Add object maper bean

--- runtime/self-hosted/src/main/java/com/cartandcook/selfhosted/configs/AiServiceFallbackConfig.java
+++ runtime/self-hosted/src/main/java/com/cartandcook/selfhosted/configs/AiServiceFallbackConfig.java
@@ -2,6 +2,8 @@ package com.cartandcook.selfhosted.configs;
 
 import com.cartandcook.core.api.AiService;
 import com.cartandcook.core.api.DisabledAiService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,13 @@ public class AiServiceFallbackConfig {
     @ConditionalOnMissingBean(AiService.class)
     public AiService disabledAiService() {
         return new DisabledAiService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
 
