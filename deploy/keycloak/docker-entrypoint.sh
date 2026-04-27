e2363b3a773cd6db3f1d838f6f5ed3e516fca29e
Jordan Ahlers <jahlers46@gmail.com>
2026-04-27 10:04:47 -0700

    Revert "Fix keycloak docker build"
    This reverts commit e020249d5fbcf496bea1c9f74e25d245c4284568.

--- deploy/keycloak/docker-entrypoint.sh
+++ deploy/keycloak/docker-entrypoint.sh
@@ -7,16 +7,8 @@ realm_output="/opt/keycloak/data/import/cooksyne-realm.json"
 : "${DOMAIN:?DOMAIN is required for Keycloak realm import}"
 : "${AUTH_CLIENT_ID:=cooksyne-ui}"
 
-escape_sed_replacement() {
-	printf '%s' "$1" | sed -e 's/[\\&|]/\\\\&/g'
-}
+export DOMAIN AUTH_CLIENT_ID
 
-domain_replacement="$(escape_sed_replacement "$DOMAIN")"
-client_id_replacement="$(escape_sed_replacement "$AUTH_CLIENT_ID")"
-
-sed \
-	-e "s|\${DOMAIN}|$domain_replacement|g" \
-	-e "s|\${AUTH_CLIENT_ID}|$client_id_replacement|g" \
-	"$realm_template" > "$realm_output"
+envsubst '${DOMAIN} ${AUTH_CLIENT_ID}' < "$realm_template" > "$realm_output"
 
 exec /opt/keycloak/bin/kc.sh "$@"
